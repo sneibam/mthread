8999b3fc6b7da623aa5c9008500f520cb9ad9c69
Mohamed Sneiba HAMOUD <mohamedsneiba.hamoud@ensiie.fr>
2019-03-10 00:03:19 +0100

    Semaphore tests

--- test/sem/sem_test_destroy.c
+++ test/sem/sem_test_destroy.c
@@ -2,21 +2,26 @@
 #include "../../mthread.h"
 
 
+mthread_sem_t sem;
+
 void *run(void *arg)
 {
+	mthread_sem_wait(&sem);
 	printf("SUCCESS\n");
+	mthread_sem_post(&sem);
 	return NULL;
 }
 
 int main(int argc, char** argv)
 {
-	// Testing the mthread_sem_init function
+	// Testing the mthread_sem_destroy function
 	mthread_t thread;
 	mthread_create(&thread, NULL, run, NULL);
-	mthread_sem_t sem;
 	printf("Initializing Semaphore...\n");
 	mthread_sem_init(&sem, 5);
 	printf("Semaphore initialized successfully\n");
 	mthread_join(thread, NULL);
+	printf("About to destroy semaphore...\n");
+	mthread_sem_destroy(&sem);
 	return 0;
 }
---
+++ test/sem/sem_test_wait_post.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "../mthread.h"
+#define NB_THREADS 10
+#define SEM_COUNTER 5
+
+mthread_sem_t sem;
+
+void* run (void* arg)
+{
+	long rank = (long)arg;
+	mthread_sem_wait(&sem);
+	printf("Coucou, je suis le thread %ld, sem counter = %d\n", rank, sem.value);
+	mthread_yield();
+	sleep(10);
+	mthread_sem_post(&sem);
+	return NULL;
+}
+
+int main(int argc, char** argv)
+{
+	int i;
+	mthread_t pids[NB_THREADS];
+	printf("Testing the Implementation of the semaphore core function (init, wait, post, destroy)\n");
+
+	mthread_sem_init(&sem,SEM_COUNTER);
+	printf("The semaphore has been initialized with a counter of %d.\n", SEM_COUNTER);
+
+	for (i = 0; i < NB_THREADS; i++) {
+		mthread_create(&(pids[i]), NULL, run, (void*)(size_t)i);
+	}
+	for (i = 0; i < NB_THREADS; i++) {
+		mthread_join(pids[i], NULL);
+	}
+
+	mthread_sem_destroy(&sem);
+
+	return 0;
+
+}
+
